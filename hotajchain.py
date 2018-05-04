e841a711266f1edd369fc33a64cf30e8858ef2cf
Eugen Hotaj <eugen_hotaj_91@hotmail.com>
2018-05-04 17:27:42 -0400

    Add concept of a Wallet to sign/verify transactions

--- hotajchain.py
+++ hotajchain.py
@@ -2,7 +2,11 @@
 
 import hashlib
 
+import ecdsa
+from ecdsa import util
 from absl import app
+
+_BITCOIN_CURVE = ecdsa.SECP256k1
 
 def _check_pow(hexdigest, difficulty):
   """Checks that the pow `hexdigest` starts wtih `difficulty` leading zeros.
@@ -23,35 +27,59 @@ def _check_pow(hexdigest, difficulty):
   return int(critical_char, 16) <= (2 ** remaining_difficulty +1) - 1
 
 
-class Block(object):
-  def __init__(self, data, prev_hash=''):
-    self._prev_hash = prev_hash
-    self._data = data
-    self._nonce = 0
+class Wallet(object):
+  def __init__(self, seed=None):
+    if seed:
+      sec_exp = util.randrange_from_seed__X(seed, _BITCOIN_CURVE.order)
+      self._sk = ecdsa.SigningKey.from_secret_exponent(sec_exp, 
+                                                        curve=_BITCOIN_CURVE)
+    else: 
+      self._sk = ecdsa.SigningKey.generate(curve=_BITCOIN_CURVE)
+    self._vk = self._sk.get_verifying_key()
+
+  def sign(self, hexdigest):
+    return self._sk.sign_digest(hexdigest)
+
+  
+  def verify(self, signature, hexdigest):
+    self._vk.verify_digest(signature, hexdigest)
+
+
+
+# TODO(ehotaj): actually implement
+class Transaction(object):
+  def __init__(self, signed_transfer, receiver_public_key):
+    self.hexdigest = self._hash() 
 
   def _hash(self):
     h = hashlib.sha256()
-    h.update(self._prev_hash.encode())
-    h.update(self._data.encode())
-    h.update(str(self._nonce).encode())
+    h.update('deadbeef'.encode())
+    return h.hexdigest()
+
+class Block(object):
+  def __init__(self, txns, prev_block_hash=''):
+    self._prev_block_hash = prev_block_hash
+    self._txns = txns
+    self._salt = 0
+
+  def _hash(self):
+    h = hashlib.sha256()
+    h.update(self._prev_block_hash.encode())
+    for txn in self._txns:
+      h.update(txn.hexdigest.encode())
+    h.update(str(self._salt).encode())
     return h.hexdigest()
 
   def pow(self, difficulty): 
     hex_ = self._hash()
     while not _check_pow(hex_, difficulty):
-      self._nonce += 1
+      self._salt += 1
       hex_ = self._hash()
 
     return hex_
 
-
-def main(argv):
-  del argv  # Unused.
-
-  genesis = Block('hello hotajchain')
-  print (genesis.pow(50))
-  print (genesis._nonce)
-
+def main():
+  print('hello')
 
 if __name__ == '__main__':
   app.run(main)
